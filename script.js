461f474d527fc5f8d84c39e7302f9d00487aa493
Komalkumari26 <168890622+Komalkumari26@users.noreply.github.com>
2024-05-31 21:43:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./dark theme icon/dark theme icon/sun.png";
+  } else {
+    icon.src = "./dark theme icon/dark theme icon/moon.png";
+  }
+};
